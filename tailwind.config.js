f121df14c2772680a8852ecf8ebc358965fbf879
NFHustle <nfhustle@outlook.com>
2022-09-10 22:53:57 -0400

    Augies World

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
     styled: true,
     themes: [
       // first one will be the default theme
-      "dark",
+      "retro",
       // uncomment to enable
       // "light (default)",
       // "dark",
